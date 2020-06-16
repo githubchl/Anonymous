53d42286fa91cc254269a62385e6a744c3430072
HLChen <2416709025@qq.com>
2020-06-16 21:48:17 +0800

    新增koa-body依赖以post上传文件支持

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ const Router = require('koa-router'); // koa 路由中间件
 const api = require("./routes/api/index.js");
 const bodyParser = require('koa-bodyparser'); // 处理post请求，把 koa2 上下文的表单数据解析到 ctx.request.body 中
 const DB = require("./module/db.js");
+const koaBody = require('koa-body');
+
 
 const app = new Koa();
 const router = new Router(); // 实例化路由
 
-app.use(bodyParser())
+app.use(koaBody({
+	multipart: true,  // 允许上传多个文件
+}));
 
-router.get("/",async (ctx, next) => {
+//app.use(bodyParser())
+
+router.post("/",async (ctx, next) => {
+	console.log(ctx.request.body);
  // let data = await DB.insert("user",{"username":"陈辉龙","password":"123456","phone":"18814379677"});
  // console.log(data.result);
  ctx.body = "Hello Anonymous";
