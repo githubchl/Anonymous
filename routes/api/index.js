1cb911853ccea6c8ec055a46f44f3a1e3136a7d8
HLChen <2416709025@qq.com>
2020-06-30 17:16:51 +0800

    新增统计app打开次数接口

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,34 @@
 let router = require('koa-router')();
 const user = require("./user.js");
+const DB = require("../../module/db.js");
 
 
 router.get('/',async (ctx,next)=> {
 	ctx.body = "哈哈哈啊";
 });
+
+
+router.post('/count',async (ctx,next)=> {
+
+    let result = await DB.find("count",{});
+    // console.log(result);
+    //更新
+    if (result&&result.length>0){
+        let count = result[0].count;
+        result = await DB.update("count",{flag:1},{flag:1,count:count+1})
+        console.log(result);
+    } else{//插入
+        result = await DB.insert("count",{flag:1,count:1})
+        //console.log(result);
+    }
+
+    ctx.body =  {
+        code: 200,
+        data: {
+            msg: '更新成功',
+        }};
+});
+
 
 router.use("/user",user.routes());
 
